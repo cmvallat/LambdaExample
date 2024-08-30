09184ef07f4795460371c64a8753c76f16b5c2cc
Christian Vallat <christianvallat02@gmail.com>
2024-08-30 15:52:52 -0400

    first commit

---
+++ lambda-dotnet6/src/HelloWorld/Host.cs
@@ -0,0 +1,11 @@
+namespace HelloWorld;
+
+public class Host
+{
+    public string username { get; set; }
+    public string party_name { get; set; }
+    public string party_code { get; set; }
+    public string phone_number { get; set; }
+    public string spotify_device_id { get; set; }
+    public int invite_only { get; set; }
+}
