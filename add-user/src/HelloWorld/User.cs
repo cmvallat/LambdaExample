23c78ebba4ed12f3477284277f5f31743d1deee2
Christian Vallat <christianvallat02@gmail.com>
2024-09-11 02:43:55 -0400

    working add-user, get-guests-from-user, get-hosts-from-user lambdas

--- add-user/src/HelloWorld/User.cs
+++ add-user/src/HelloWorld/User.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace HelloWorld;
 
 public class User
 {
     public string username { get; set; }
-    public string phone_number { get; set; }
-    public string password { get; set; }
+    public string email { get; set; }
+    public Guid cognito_username { get; set; }
 }
